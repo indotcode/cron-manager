3548905cc771d7d81a3f7cec5eba84b3bf39ca6d
indotcode <oruno999@gmail.com>
2022-01-02 07:14:35 +0700

    Настройка Vue Laravel Mix

--- source/app.js
+++ source/app.js
@@ -2,6 +2,5 @@ import { createApp } from 'vue';
 import App from './components/App.vue';
 
 const app = createApp(App);
-app.config.performance = true;
 app.config.devtools = true
 app.mount('#cronManager');
--- webpack/prod.mix.js
+++ webpack/prod.mix.js
@@ -1,5 +1,6 @@
 let mix = require('laravel-mix');
 
+mix.disableNotifications();
 mix.js('source/app.js', 'assets/js').vue({version: 3});
 mix.sass('source/app.scss', 'assets/css', {}, [
     require('tailwindcss')
