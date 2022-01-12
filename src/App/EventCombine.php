0130b97aab32aa11a72f0f634ca436600ef786e7
indotcode <oruno999@gmail.com>
2022-01-13 02:42:04 +0700

    Добавлено описание

--- src/App/EventCombine.php
+++ src/App/EventCombine.php
@@ -10,15 +10,14 @@ class EventCombine
 {
     public function launch(Schedule $schedule) : void
     {
-
+        $eventCl = new Event();
         $events = CronManagerEvent::where('active', 1)->get();
         foreach ($events as $event){
             $event_method = $event['methods'];
             $eventF = new EventFactories($schedule);
-            $eventF->setCall(function () use ($event, $event_method) {
+            $eventF->setCall(function () use ($event, $event_method, $eventCl) {
                 $log = new LogFactories();
                 try {
-                    $eventCl = new Event();
                     $eventCl->$event_method($event);
                     $log->setLog($event, 'Задание успешно завершено.');
                 } catch (\Exception $e){
